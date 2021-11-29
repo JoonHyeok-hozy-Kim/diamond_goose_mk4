02075fda840e9e6598c1ce659b73178a74ffffdb
hozy <danielisdan@naver.com>
2021-11-29 14:57:23 +0900

    equityowned app average_purchase price logic developed

--- equityownedapp/models.py
+++ equityownedapp/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Q
+from copy import deepcopy
 
 # Create your models here.
 from equitymasterapp.models import Equity
@@ -15,3 +17,76 @@ class EquityOwned(models.Model):
 
     creation_date = models.DateTimeField(auto_now=True)
     last_update_date = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def quantity(self):
+        query = Q(transaction_type='BUY')
+        query.add(Q(transaction_type='SELL'),Q.OR)
+        query.add(Q(quantity__gt=0),Q.AND)
+        query.add(Q(split_flag=False),Q.AND)
+
+        transaction_data_set = self.transaction.filter(query).values()
+        result = 0
+
+        for transaction_data in transaction_data_set:
+            if transaction_data['transaction_type'] == 'BUY':
+                result += transaction_data['quantity']
+            else:
+                result -= transaction_data['quantity']
+
+        return result
+
+    @property
+    def average_purchase_price_mv(self):
+        query = Q(transaction_type='BUY')
+        query.add(Q(transaction_type='SELL'),Q.OR)
+        query.add(Q(quantity__gt=0),Q.AND)
+        query.add(Q(split_flag=False),Q.AND)
+
+        transaction_data_set = self.transaction.filter(query).order_by('transaction_date').values()
+        temp_qty = 0
+        temp_amt = 0
+
+        for transaction_data in transaction_data_set:
+            if transaction_data['transaction_type'] == 'BUY':
+                temp_qty += transaction_data['quantity']
+                temp_amt += transaction_data['quantity'] * transaction_data['price']
+            else:
+                temp_price = temp_amt/temp_qty
+                temp_qty -= transaction_data['quantity']
+                temp_amt = temp_qty * temp_price
+
+        if temp_qty < 0: result = -999
+        elif temp_qty == 0: result = 0
+        else: result = temp_amt/temp_qty
+
+        return result
+
+    @property
+    def average_purchase_price_fifo(self):
+        query = Q(transaction_type='BUY')
+        query.add(Q(transaction_type='SELL'),Q.OR)
+        query.add(Q(quantity__gt=0),Q.AND)
+        query.add(Q(split_flag=False),Q.AND)
+
+        transaction_data_set = self.transaction.filter(query).order_by('transaction_date').values()
+        transaction_amount_list = []
+        temp_qty = 0
+        temp_amt = 0
+
+        for transaction_data in transaction_data_set:
+            if transaction_data['transaction_type'] == 'BUY':
+                for i in range(int(transaction_data['quantity'])):
+                    transaction_amount_list.append(transaction_data['price'])
+            else:
+                for i in range(int(transaction_data['quantity'])):
+                    transaction_amount_list.pop(0)
+
+        for transaction_amount in transaction_amount_list:
+            temp_qty += 1
+            temp_amt += transaction_amount
+
+        if temp_qty > 0: result = temp_amt/temp_qty
+        else: result = 0
+
+        return result
--- equitytransactionapp/models.py
+++ equitytransactionapp/models.py
@@ -26,11 +26,17 @@ class EquityTransaction(models.Model):
 
     transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPE_CHOICES, null=False)
     quantity = MinValueFloat(min_value=0.0, null=False)
-    purchase_price = models.FloatField(null=False)
+
+    # Not-in-use. Substituted by price
+    purchase_price = models.FloatField(default=0,null=False)
+
+    price = MinValueFloat(min_value=0.0, default=0, null=False)
     transaction_fee = models.FloatField(default=0)
     transaction_tax = models.FloatField(default=0)
     transaction_date = models.DateTimeField(default=utils.timezone.now, null=False)
-    note = models.CharField(max_length=20, null=True)
+    note = models.CharField(max_length=20, default=' -', null=True)
+
+    split_flag = models.BooleanField(default=False)
 
     creation_date = models.DateTimeField(auto_now=True)
     last_update_date = models.DateTimeField(auto_now_add=True)
